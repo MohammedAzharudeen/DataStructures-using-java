8b2583136c2fa9a865883cd6f11eb969f8e6bee4
Mohammed Azharudeen A <42969244+MohammedAzharudeen@users.noreply.github.com>
2021-05-30 21:10:46 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -9,12 +9,15 @@ public class Node {
                     
 public class Runner {
 
+	public class Runner {
+
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		list.insert(18);
 		list.insert(45);
 		list.insert(12);
-		
+		list.insertAtStart(25);
+		list.insertAt(0,55);
 		list.show();
 	}
 
@@ -45,6 +48,36 @@ public class LinkedList
 			}
 			n.next = node;
 		}
+	}
+	
+	public void insertAtStart(int data)         //insert at beginning
+	{
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+		
+		node.next = head;
+		head = node;
+	}
+	
+	void insertAt(int index,int data)           //insert at a position
+	{
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+		
+		if(index==0)
+		{
+			insertAtStart(data);
+			return;
+		}
+		Node n = head;
+		for(int i=0;i<index-1;i++)
+		{
+			n = n.next;
+		}
+		node.next = n.next;
+		n.next = node;
 	}
 	
 	public void show()                                    //to print the list.
