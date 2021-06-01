bf98ca20dbb51c0b5ad9764d71fd9fa337b87098
Mohammed Azharudeen A <42969244+MohammedAzharudeen@users.noreply.github.com>
2021-06-01 21:14:13 +0530

    Update Stack ArrayImplementation.java

--- Stack ArrayImplementation.java
+++ Stack ArrayImplementation.java
@@ -4,15 +4,17 @@ public class Runner {
 
 	public static void main(String[] args) {
 		Stack nums = new Stack();
+		System.out.println("Empty : "+nums.isEmpty());
 		nums.push(15);
 		nums.push(8);
 		nums.show();
-		System.out.println("\nPeek element is : " + nums.peek());
-
 		nums.push(10);
 		nums.show();
+		System.out.println("\nPeek element is : " + nums.peek());
 		System.out.println("\nPopped element is : " + nums.pop());
-		
+		System.out.println("\nSize is : "+nums.size());                      //size
+		System.out.println("Popped element is : " + nums.pop());
+		System.out.println("Empty : "+nums.isEmpty());                        //empty or not
 		nums.show();
 	}
 
@@ -29,15 +31,23 @@ public class Stack {
 	int top = 0;
 	
 	public void push(int data) {
+		if(top == 5) {
+			System.out.println("Stack is full!");
+			return;
+		}
 		stack[top] = data;
 		top++;
 	}
 	
 	public int pop() {
-		int data;
-		top--;
-		data = stack[top];
-		stack[top] = 0;
+		int data=0;
+		if(isEmpty()) {
+			System.out.println("Stack is empty,cannot pop");
+		}else {
+			top--;
+			data = stack[top];
+			stack[top] = 0;
+		}
 		return data;
 	}
 	
@@ -47,9 +57,18 @@ public class Stack {
 		return data;
 	}
 	
+	public int size() {                                   //to check size.
+		return top;
+	}
+	
+	public boolean isEmpty() {                             //check whether the stack is empty by returning true or false
+		return top<=0;
+	}
+	
 	public void show() {
 		for( int n : stack) {
 			System.out.print(n + " ");
 		}
+		System.out.println();
 	}
 }
