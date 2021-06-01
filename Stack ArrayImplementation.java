9eb96978d5ad933a152e8a4d4e25420d9bae7d3f
Mohammed Azharudeen A <42969244+MohammedAzharudeen@users.noreply.github.com>
2021-06-01 14:55:53 +0530

    Update Stack ArrayImplementation.java

--- Stack ArrayImplementation.java
+++ Stack ArrayImplementation.java
@@ -1,1 +1,55 @@
+package DSstack;
 
+public class Runner {
+
+	public static void main(String[] args) {
+		Stack nums = new Stack();
+		nums.push(15);
+		nums.push(8);
+		nums.show();
+		System.out.println("\nPeek element is : " + nums.peek());
+
+		nums.push(10);
+		nums.show();
+		System.out.println("\nPopped element is : " + nums.pop());
+		
+		nums.show();
+	}
+
+}
+
+
+                  /*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+public class Stack {
+
+	int stack[] = new int[5];
+	int top = 0;
+	
+	public void push(int data) {
+		stack[top] = data;
+		top++;
+	}
+	
+	public int pop() {
+		int data;
+		top--;
+		data = stack[top];
+		stack[top] = 0;
+		return data;
+	}
+	
+	public int peek() {
+		int data;
+		data = stack[top-1];
+		return data;
+	}
+	
+	public void show() {
+		for( int n : stack) {
+			System.out.print(n + " ");
+		}
+	}
+}
