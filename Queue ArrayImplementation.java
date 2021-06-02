69948c5a8b25e4438f4c258bd01afb9e3d787a2a
Mohammed Azharudeen A <42969244+MohammedAzharudeen@users.noreply.github.com>
2021-06-02 21:52:45 +0530

    Create Queue ArrayImplementation.java

---
+++ Queue ArrayImplementation.java
@@ -0,0 +1,41 @@
+package QueueJava;
+public class Runner {
+
+	public static void main(String[] args) {
+		Queue q = new Queue();
+		q.enQueue(5);
+		q.enQueue(7);
+		q.enQueue(9);
+		q.show();
+
+	}
+
+}
+
+
+
+
+                    /*---------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+public class Queue {
+	int[] queue = new int[5];
+	int size;
+	int front;
+	int rear;
+	
+	public void enQueue(int data) {
+		queue[rear] = data;
+		rear++;
+		size++;
+	}
+	
+	public void show() {
+		System.out.print("Elements : ");
+		for(int i=0;i<size;i++) {
+			System.out.print(queue[i]+" ");
+		}
+	}
+}
