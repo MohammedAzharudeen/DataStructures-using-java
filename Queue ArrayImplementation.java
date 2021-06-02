a09c636b142e8b507709f1f69413465875ba00de
Mohammed Azharudeen A <42969244+MohammedAzharudeen@users.noreply.github.com>
2021-06-02 22:36:05 +0530

    Update Queue ArrayImplementation.java

--- Queue ArrayImplementation.java
+++ Queue ArrayImplementation.java
@@ -3,9 +3,19 @@ public class Runner {
 
 	public static void main(String[] args) {
 		Queue q = new Queue();
+		System.out.println(q.isEmpty());
 		q.enQueue(5);
+		q.enQueue(2);
 		q.enQueue(7);
+		q.enQueue(3);
+		
+		q.deQueue();
+		q.deQueue();
+		
 		q.enQueue(9);
+		q.enQueue(1);
+		System.out.println(q.isFull());
+		System.out.println("Size : "+q.getSize());
 		q.show();
 
 	}
@@ -27,15 +37,44 @@ public class Queue {
 	int rear;
 	
 	public void enQueue(int data) {
-		queue[rear] = data;
-		rear++;
-		size++;
+		
+		if(!isFull()) {
+			queue[rear] = data;
+			rear = (rear + 1) % 5;
+			size = size + 1;
+		}else {
+			System.out.println("Queue is full!");
+		}
+	}
+	
+	public int deQueue() {
+		int data = queue[front];
+		if(!isEmpty()) {
+			front = (front + 1) % 5;
+			size = size - 1;
+		}else {
+			System.out.println("Queue is empty!");
+		}
+		return data;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return getSize() == 0;
+	}
+	
+	public boolean isFull() {
+		return getSize() == 5;
 	}
 	
 	public void show() {
 		System.out.print("Elements : ");
 		for(int i=0;i<size;i++) {
-			System.out.print(queue[front+i]+" ");
+			System.out.print(queue[(front+i)%5]+" ");
 		}
+		System.out.println();
 	}
 }
